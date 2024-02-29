257a64d8854f873ff4a8ab91ee0f4b0775d38041
DannyClark90 <dannyclark_90@hotmail.co.uk>
2024-02-29 19:07:56 +0000

    Project architecture set up.

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Sass/Components/_header.scss'
+
+export default function Header() {
+  return (
+    <header className="header">
+
+    </header>
+  )
+};
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Sass/Components/_navbar.scss'
+
+export default function Navbar() {
+  return (
+    <>
+    <nav className='navbar'>
+        NAVBAR
+    </nav>
+    </>
+  )
+}
