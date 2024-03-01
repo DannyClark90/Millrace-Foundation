9c7e630e627549c899b232dc3510c2b43e5d2ca6
DannyClark90 <dannyclark_90@hotmail.co.uk>
2024-03-01 16:38:45 +0000

    Header complete.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,13 @@ import '../Sass/Components/_header.scss'
 export default function Header() {
   return (
     <header className="header">
-
+      <div className="header__bg-img">
+        <h1 className="heading-primary">
+          <span class="heading-primary--main">Welcome to The millrace foundation</span>
+          <span class="heading-primary--sub">Action On Mental Health</span>
+        </h1>
+      </div>
+      <img className='header__logo' src="src/assets/Millrace Outline mod.png" alt="" />
     </header>
   )
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,8 @@ import Header from '../Components/Header'
 export default function Home() {
   return (
     <>
-       <Header/> 
+       <Header/>
+   
     </>
   )
 }
