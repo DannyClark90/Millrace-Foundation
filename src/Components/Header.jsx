b02b63c173a1a96dde93ec2b6ace2e787b3df9ff
DannyClark90 <dannyclark_90@hotmail.co.uk>
2024-03-05 14:07:53 +0000

    Nav & header responsive design added.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import '../Sass/Layout/_header.scss'
+import React from "react";
+import "../Sass/Layout/_header.scss";
 
 export default function Header() {
   return (
-    <header className="header">
-      <div className="header__bg-img">
+    <>
+      <header className="header">
         <h1 className="heading-primary">
-          <span class="heading-primary--main">Welcome to The millrace foundation</span>
-          <span class="heading-primary--sub">Action On Mental Health</span>
+          <span className="heading-primary--main">
+            Welcome to The millrace foundation
+          </span>
+          <span className="heading-primary--sub">Action On Mental Health</span>
         </h1>
-      </div>
-      <img className='header__logo' src="src/assets/Millrace Outline mod.png" alt="" />
-    </header>
-  )
-};
+      </header>
+    </>
+  );
+}
