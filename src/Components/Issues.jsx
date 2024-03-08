2187128a8a194c29b7b9afe5c8f3115d659e8d93
DannyClark90 <dannyclark_90@hotmail.co.uk>
2024-03-08 16:50:33 +0000

    issues section content added.

--- src/Components/Issues.jsx
+++ src/Components/Issues.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React from "react";
 import "../Sass/Components/_issues.scss";
 import "../Sass/Base/_utilities.scss";
+import { motion } from "framer-motion";
 
 export default function Issues() {
   return (
     <section className="issues">
-        <h2 className="section-heading">The Issues</h2>
-        <img className='issues-img' src="src/assets/Issues.png" alt="Depressed looking person" />
-        <p></p>
+      <h2 className="section-heading margin-bottom-4">The Issues</h2>
+      <div className="issues__grid">
+        <img
+          className="issues__img"
+          src="src/assets/Issues.png"
+          alt="Depressed looking person"
+        />
+
+        <div className="issues__card">
+          <p>
+            4 to 10% of people of Essex will experience mental illness in their
+            lifetime.
+          </p>
+        </div>
+
+        <div className="issues__card">
+          <p>
+            Common mental health problems such as depression, anxiety and
+            suicidal thoughts can affect you and your family at anytime.
+          </p>
+        </div>
+
+        <div className="issues__card">
+          <p>
+            Mixed anxiety and depression have been estimated to cause one fifth
+            of days lost from work in the UK. One of the major causes of social
+            isolation today.
+          </p>
+        </div>
+
+        <div className="issues__card">
+          <p>
+            High rates of suicide exist currently in our local community and in
+            some areas above the national average.
+          </p>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
