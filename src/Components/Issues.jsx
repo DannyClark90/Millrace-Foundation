81c4c87ca492f3c00ad7c749d3fa8fee70fe3e98
DannyClark90 <dannyclark_90@hotmail.co.uk>
2024-03-08 17:36:24 +0000

    issues section animation added.

--- src/Components/Issues.jsx
+++ src/Components/Issues.jsx
@@ -1,47 +1,95 @@
 import React from "react";
 import "../Sass/Components/_issues.scss";
 import "../Sass/Base/_utilities.scss";
-import { motion } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
 
 export default function Issues() {
+  const animationVariants = {
+    initial: { y: 100, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+    transition: { duration: 2 },
+  };
+
   return (
     <section className="issues">
-      <h2 className="section-heading margin-bottom-4">The Issues</h2>
+      <motion.h2
+        className="section-heading margin-bottom-4"
+        variants={animationVariants}
+        initial="initial"
+        transition={{ duration: 0.8 }}
+        whileInView="animate"
+        viewport={{ once: true }}
+      >
+        The Issues
+      </motion.h2>
       <div className="issues__grid">
-        <img
+        <motion.img
           className="issues__img"
           src="src/assets/Issues.png"
           alt="Depressed looking person"
+          variants={animationVariants}
+          initial="initial"
+          transition={{ duration: 0.8 }}
+          whileInView="animate"
+          viewport={{ once: true }}
         />
 
-        <div className="issues__card">
+        <motion.div
+          className="issues__card"
+          variants={animationVariants}
+          initial="initial"
+          transition={{ duration: 0.8 }}
+          whileInView="animate"
+          viewport={{ once: true }}
+        >
           <p>
             4 to 10% of people of Essex will experience mental illness in their
             lifetime.
           </p>
-        </div>
+        </motion.div>
 
-        <div className="issues__card">
+        <motion.div
+          className="issues__card"
+          variants={animationVariants}
+          initial="initial"
+          transition={{ duration: 0.8 }}
+          whileInView="animate"
+          viewport={{ once: true }}
+        >
           <p>
             Common mental health problems such as depression, anxiety and
             suicidal thoughts can affect you and your family at anytime.
           </p>
-        </div>
+        </motion.div>
 
-        <div className="issues__card">
+        <motion.div
+          className="issues__card"
+          variants={animationVariants}
+          initial="initial"
+          transition={{ duration: 0.8 }}
+          whileInView="animate"
+          viewport={{ once: true }}
+        >
           <p>
             Mixed anxiety and depression have been estimated to cause one fifth
             of days lost from work in the UK. One of the major causes of social
             isolation today.
           </p>
-        </div>
+        </motion.div>
 
-        <div className="issues__card">
+        <motion.div
+          className="issues__card"
+          variants={animationVariants}
+          initial="initial"
+          transition={{ duration: 0.8 }}
+          whileInView="animate"
+          viewport={{ once: true }}
+        >
           <p>
             High rates of suicide exist currently in our local community and in
             some areas above the national average.
           </p>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
